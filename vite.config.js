6c8d349deda974f85667b032361dc204dbec0e48
justyaviz <justyaviz@gmail.com>
2026-03-24 17:47:47 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    host: "0.0.0.0"
   },
   preview: {
-    host: true,
-    allowedHosts: "all"
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 8080,
+    allowedHosts: [
+      "spectacular-reverence-production.up.railway.app"
+    ]
   }
 });
