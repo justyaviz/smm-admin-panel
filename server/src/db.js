e76e754533e7f09fe9dfbdc7309d38307aa6ee17
justyaviz <justyaviz@gmail.com>
2026-03-25 10:16:03 +0500

    Update db.js

--- server/src/db.js
+++ server/src/db.js
@@ -1,9 +1,13 @@
-import pkg from 'pg';
-const { Pool } = pkg;
+import pg from "pg";
+
+const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL?.includes('localhost') ? false : { rejectUnauthorized: false }
+  ssl:
+    process.env.DATABASE_URL && process.env.DATABASE_URL.includes("railway.app")
+      ? { rejectUnauthorized: false }
+      : false
 });
 
 export async function query(text, params = []) {
