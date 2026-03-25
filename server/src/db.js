c1f5fc7bf3f96ae3c970c747d2e9d39546ab6320
justyaviz <justyaviz@gmail.com>
2026-03-25 13:57:20 +0500

    Update db.js

--- server/src/db.js
+++ server/src/db.js
@@ -5,7 +5,9 @@ const { Pool } = pg;
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl:
-    process.env.DATABASE_URL && process.env.DATABASE_URL.includes("railway.app")
+    process.env.DATABASE_URL &&
+    (process.env.DATABASE_URL.includes("railway.app") ||
+      process.env.DATABASE_URL.includes("proxy.rlwy.net"))
       ? { rejectUnauthorized: false }
       : false
 });
